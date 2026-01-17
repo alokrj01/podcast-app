85ef937cf800a3c49ed8f5974fc70690279f469e
alokrj01 <ranjanalok2468@gmail.com>
2026-01-17 15:55:38 +0530

    implmented backend and authentication

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,14 +1,18 @@
 import { AuthConfig } from "convex/server";
 
+const clerkDomain = process.env.CLERK_JWT_ISSUER_DOMAIN;
+
+if (!clerkDomain) {
+  throw new Error(
+    "Missing required environment variable: CLERK_JWT_ISSUER_DOMAIN"
+  );
+}
+
 export default {
   providers: [
     {
-      // Replace with your own Clerk Issuer URL from your "convex" JWT template
-      // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
-      // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
-      // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-      domain: "https://active-cat-16.clerk.accounts.dev",
+      domain: clerkDomain,
       applicationID: "convex",
     },
-  ]
+  ],
 } satisfies AuthConfig;
